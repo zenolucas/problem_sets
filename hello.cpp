2fc801c9994f70dece305fd72a8b782b863f5661
jhunisamae <147142851+jhunisamae@users.noreply.github.com>
2023-12-18 05:23:33 +0800

    Delete hello.cpp

--- hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    // Print "Hello, World!" to the console
-    std::cout << "Hello, World!" << std::endl;
-
-    // Return 0 to indicate successful completion
-    return 0;
-}
