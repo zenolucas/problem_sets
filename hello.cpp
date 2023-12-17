4c5362e3d0fd314df3536429de94c63c7103d02b
jhunisamae <147142851+jhunisamae@users.noreply.github.com>
2023-12-18 05:01:51 +0800

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // Print "Hello, World!" to the console
+    std::cout << "Hello, World!" << std::endl;
+
+    // Return 0 to indicate successful completion
+    return 0;
+}
